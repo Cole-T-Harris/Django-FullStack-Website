4004fc05e824b76316c5bfb495f52f66c7d52429
Cole-T-Harris <cole.t.harr@gmail.com>
2023-05-10 19:46:30 -0600

    Add dropdown to filter on radius for location selection

--- django_project/GroceryApp/src/Components/Stores/StorePicker.js
+++ django_project/GroceryApp/src/Components/Stores/StorePicker.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Dropdown from 'react-bootstrap/Dropdown'
 import DropdownButton from 'react-bootstrap/DropdownButton'
-import InputGroup from 'react-bootstrap'
+import InputGroup from 'react-bootstrap/InputGroup'
 import "react-icons/fa"
 import { useLocations } from '../../hooks/useLocations'
 
 export default function StorePicker(props) {
   const [zipCode, setZipCode] = useState("")
-  const [radius, setRadius] = useState(10)
+  const [radius, setRadius] = useState(50)
   const {locations, setLocations, invalidZipCode} = useLocations(zipCode, radius)
   const locationProps = {
     zipCode, setZipCode,
@@ -25,23 +25,44 @@ export default function StorePicker(props) {
 function StoreFinderForm(props) {
   return (
     <div className="store-input-width">
-      <Form>
-        <Form.Group className='mb-3'>
-          <Form.Control 
-            type="search"
-            placeholder='Enter your zipcode'
-            value={props.zipCode}
-            onChange={(input) => props.setZipCode(input.target.value)}
-            isInvalid={props.invalidZipCode}
-          />
-          <Form.Control.Feedback type="invalid">
-            Please enter a valid 5 digit zipcode.
-          </Form.Control.Feedback>
-          <Form.Text className="text-muted">
-            Search for grocery stores near your zipcode.
-          </Form.Text>
-        </Form.Group>
-      </Form>
+      <InputGroup hasValidation>
+        <Form.Control 
+          type="search"
+          placeholder='Enter your zipcode'
+          value={props.zipCode}
+          onChange={(input) => props.setZipCode(input.target.value)}
+          isInvalid={props.invalidZipCode}
+        >
+        </Form.Control>
+        <Form.Control.Feedback type="invalid">
+          Please enter a valid 5 digit zipcode.
+        </Form.Control.Feedback>
+      </InputGroup>
+      <StoreRadiusFilter {...props}/>
     </div>
+  )
+}
+
+function StoreRadiusFilter(props) {
+  const dropDownLabel = props.radius + "miles"
+  const handleSelect = (eventKey) => {
+    props.setRadius(eventKey)
+  }
+  return (
+    <>
+      Seach Within
+        <DropdownButton
+        variant="light"
+        title={dropDownLabel}
+        size="sm"
+        onSelect={handleSelect}
+      >
+        <Dropdown.Item eventKey={5}>5 miles</Dropdown.Item>
+        <Dropdown.Item eventKey={10}>10 miles</Dropdown.Item>
+        <Dropdown.Item eventKey={15}>15 miles</Dropdown.Item>
+        <Dropdown.Item eventKey={25}>25 miles</Dropdown.Item>
+        <Dropdown.Item eventKey={50}>50 miles</Dropdown.Item>
+      </DropdownButton>
+    </>
   )
 }
