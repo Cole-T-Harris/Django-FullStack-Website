f5ecb1c2e91f35b2ee1c79240197af9723bf2eca
Cole-T-Harris <cole.t.harr@gmail.com>
2023-09-05 15:35:28 -0600

    Add table for item search returned

--- django_project/GroceryApp/src/Components/List/ItemSearchModal.js
+++ django_project/GroceryApp/src/Components/List/ItemSearchModal.js
@@ -4,13 +4,15 @@ import InputGroup from 'react-bootstrap/InputGroup'
 import Form from 'react-bootstrap/Form'
 import Modal from 'react-bootstrap/Modal'
 import { useItems } from "../../hooks/useItems";
+import Table from 'react-bootstrap/Table';
+import Image from 'react-bootstrap/Image';
+import { Loading } from '../../utils/constants'
 
 export default function ItemSearchModal(props) {
     const handleClose = () => props.setShowItemSearch(false)
     const [searchPressed, setSearchPressed] = useState(false)
     const [itemSearchTerm, setItemSearchTerm] = useState("")
-    const {items, setItems, totalResults, loading} = useItems(props.storeID, itemSearchTerm, 0, searchPressed)
-    console.log(items, totalResults, loading)
+    const itemsResponse = useItems(props.storeID, itemSearchTerm, 0, searchPressed)
     return (
         <Modal 
             show={props.showItemSearch}
@@ -21,6 +23,7 @@ export default function ItemSearchModal(props) {
             </Modal.Header>
             <Modal.Body>
                 <GroceryListSearch term={itemSearchTerm} setTerm={setItemSearchTerm} />
+                <ItemSearchResultsTable {...itemsResponse} />
             </Modal.Body>
             <Modal.Footer>
                 <Button
@@ -46,5 +49,88 @@ function GroceryListSearch(props) {
 }
 
 function ItemSearchResultsTable(props) {
+    if (props.loading) {
+        return (
+            <Loading/>
+        )
+    }
+    return(
+        <Table striped>
+            <thead></thead>
+            <tbody>
+                {props.items.map((product, index) => (
+                    <ItemSearchResultsTableRow
+                        product={product}
+                        key={`table-${JSON.stringify(product)}`}
+                    />
+                ))}
+            </tbody>
+        </Table>
+    )
+}
 
+function ItemSearchResultsTableRow(props) {
+    const product = props.product
+    return (
+        <tr>
+            <td>
+                <Button 
+                    variant="secondary"
+                    size="sm"
+                >Add
+                </Button>
+                <ItemStockLevel stock={product.stock}/>
+            </td>
+            <td>
+                <Image src={product?.thumbnail} />
+            </td>
+            <td>
+                {product?.description}
+            </td>
+            <td>
+                <ItemDisplayPrice price={product.price} promo={product.promo} priceSize={product.priceSize}/>
+            </td>
+        </tr>
+    )
+}
+
+function ItemDisplayPrice(props) {
+    if (props.promo > 0) {
+        return (
+            <p>
+                <del style={{color: "red"}}>${props.price}</del>
+                <span style={{color: "red"}}>${props.promo}</span>
+                / {props.priceSize}
+            </p>
+        )
+    }
+    if (props.price > 0) {
+        return (
+            <p>${props.price} / {props.priceSize}</p>
+        )
+    }
+    return (
+        <p>-</p>
+    )
+}
+
+function ItemStockLevel(props) {
+    if (props.stock == "HIGH") {
+        return (
+            <span style={{color: "green"}}> In Stock</span>
+        )
+    }
+    if (props.stock == "LOW") {
+        return (
+            <span style={{color: "orange"}}> Low Stock</span>
+        )
+    }
+    if (props.stock == "TEMPORARILY_OUT_OF_STOCK") {
+        return (
+            <span style={{color: "red"}}> Out of Stock</span>
+        )
+    }
+    return (
+        <p></p>
+    )
 }
